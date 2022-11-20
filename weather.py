aaac7baa78b05e48888f12514944a145023c574e
parham taki <parhamtaki85@gmail.com>
2022-11-20 23:05:44 +0330

    cleaning
    don't lose your hope everyone at beginning have a lot of problem (even me, my problems were worse and have a lot of systax error sometime they ran just by lucky)
    your code doesn't seems have any serious problem except line 27 can be attacked by input injection but don't mind it cuz it'll run on their system not your system

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
-from cProfile import label
 import requests
 import tkinter as tk
-from turtle import title 
+from math import floor # or ceil # round is a built-in tool
+
 window = tk.Tk()
 window.geometry("300x404+200+200")
 window.maxsize(500,600)
@@ -9,32 +9,35 @@ window.minsize(200,300)
 #*********************************************************api*************************************************
 
 #*********************************************************image***********************************************
-img = tk.PhotoImage(file="/home/ali/Desktop/project/python/uuu.png")
+img = tk.PhotoImage(file="./uuu.png")  # it's better to use os.path.join(os.directory(__file__), "uuu.png") but forget which I wrote
 img_show = tk.Label(image = img)
 #*********************************************************title***********************************************
 window.title("weather")
 #********************************************************defind***********************************************
 def get_name():
-    
     name = entry_1.get()
     city = entry_2.get()
     city = city.lower()
     print(f"name is {name} and city is {city}")
-    window.destroy()
-    window_2 = tk.Tk()
+    window_2 = tk.TopLevel(window) # in this case user doesn't need to restart program to check another city
     window_2.geometry("200x200")
     window_2.maxsize(150,200)
     window_2.minsize(150,200)
-    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06c921750b9a82d8f5d1294e1586276f"
+    # readablity of f string is better than `+` operator
+    api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=06c921750b9a82d8f5d1294e1586276f" 
     res = requests.get(api)
     data = res.json()
     condition = data['weather'][0]['main']
-    temp = int(data['main']['temp'] - 273.15)
-    final_info = condition + "\n" + str(temp) + "°C"
-    lable_3 = tk.Label(window_2,text=final_info).pack()
-    button_3 = tk.Button(window_2,text="exit",bg="khaki",fg="BLACK",command=exit,width=40,height=2).pack()
-    if __name__ == "__main__":
-        window_2.mainloop()
+    """int in bigger than zero numbers makes the number smaller and in smaller than zero numbers make  the 
+    number bigger, so that use floor (for make it smaller), ceil (for make it bigger), and round (to make 
+    bigger or smaller depends on the decimal part) instead"""
+    temp = floor(data['main']['temp'] - 273.15) # or ceil(data['main']['temp'] - 273.15) or round(data['main']['temp'] - 273.15)   
+    final_info = f"{condition}\n{temp}°C"  # readablity of f string is better than `+` operator
+    lable_3 = tk.Label(window_2,text=final_info)
+    button_3 = tk.Button(window_2,text="exit",bg="khaki",fg="BLACK",command=exit,width=40,height=2) # you should change this
+    button_3.pack()
+    label_3.pack()
+    
 #*********************************************************frame***********************************************
 frame_1 = tk.Frame(window)
 frame_2 = tk.Frame(window)
@@ -58,12 +61,6 @@ label_2.pack()
 entry_2.pack()
 button_1.pack()
 button.pack()
-
-
-
-
-
-
 
 if __name__ =="__main__":
     window.mainloop()
